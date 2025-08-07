568becae271a326e13865c0263940f232a774222
Natalia Ivanova <nataliaivanova@MacBookPro.fritz.box>
2025-08-07 13:34:14 +0200

    Update constants.js to use the environment

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,6 @@
-export const API_URL = 'http://localhost:3001/posts';
-export const API_URL2 = 'http://localhost:3001';
+export const API_URL =
+  (import.meta.env.VITE_API_URL || 'http://localhost:3001') + '/posts';
+export const API_URL2 = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+
+// export const API_URL = 'http://localhost:3001/posts';
+// export const API_URL2 = 'http://localhost:3001';
